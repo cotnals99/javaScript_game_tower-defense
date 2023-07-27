589552b14d54371fc6e71ab90e397b8b4067f1e4
cotnals99 <su.chae@icloud.com>
2023-07-27 11:29:11 +0700

    Enemy movement basic, waypoints calculation

--- js/index.js
+++ js/index.js
@@ -14,15 +14,73 @@ image.onload = () => {
 }
 image.src = '../img/gameMap.png'
 
-let x = 200
+class Enemy {
+    constructor({position, width, height}){
+        this.position = {
+            x: position.x,
+            y: position.y
+        },
+        this.width = width,
+        this.height = height,
+        this.waypointIndex = 0,
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
+    }
+
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+        this.draw()
+
+        const waypoint = waypoints[this.waypointIndex]
+        const yDistance = waypoint.y - this.center.y
+        const xDistance = waypoint.x - this.center.x
+        const angle = Math.atan2(yDistance, xDistance)
+        // console.log(this.position.x)
+
+        this.position.x += Math.cos(angle)
+        this.position.y += Math.sin(angle)
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
+
+
+        if (Math.round(this.center.x) === Math.round(waypoint.x) && 
+            Math.round(this.center.y) === Math.round(waypoint.y) && 
+            this.waypointIndex < waypoints.length
+            ){
+            this.waypointIndex++
+        }
+
+
+    }
+}
+
+// let x = 200
+
+const enemy = new Enemy({
+    position: {
+        x: waypoints[0].x,
+        y: waypoints[0].y
+    },
+    width: 100,
+    height: 100
+})
 
 function animate(){
     window.requestAnimationFrame(animate)
-
     c.drawImage(image, 0, 0)
 
-    c.fillStyle='red'
-    c.fillRect(x, 400, 100, 100)
+    enemy.update()
+
+    // c.fillStyle='red'
+    // c.fillRect(x, 400, 100, 100)
     // x++
     // console.log('go')
 }
