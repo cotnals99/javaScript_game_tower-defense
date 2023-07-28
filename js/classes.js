cf1a56a4318fd9924aefd6f51660f35150f5b7d4
cotnals99 <su.chae@icloud.com>
2023-07-28 09:03:04 +0700

    FIX: duplicate selecting, Increase building size

---
+++ js/classes.js
@@ -0,0 +1,95 @@
+// Placement Tiles
+// console.log(placementTilesData)
+
+class PlacementTile {
+    constructor({ position = { x: 0, y: 0 } }) {
+      this.position = position
+      this.size = 64
+      this.color = 'rgba(255, 255, 255, 0.15)'
+      this.isOccupied = false
+    }
+  
+    draw() {
+      c.fillStyle = this.color
+      c.fillRect(this.position.x, this.position.y, this.size, this.size)
+    }
+
+    update(mouse){
+        this.draw()
+
+        if(
+            mouse.x > this.position.x &&
+            mouse.x < this.position.x + this.size &&
+            mouse.y > this.position.y &&
+            mouse.y < this.position.y + this.size
+            ){
+                console.log('Collide')
+                this.color = 'white'
+            }
+        else this.color = 'rgba(255, 255, 255, 0.15)'
+    }
+  }
+
+
+//Enemy
+class Enemy {
+    constructor({position, width, height}){
+        this.position = {
+            x: position.x,
+            y: position.y
+        },
+        this.width = width,
+        this.height = height,
+        this.waypointIndex = 0,
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
+    }
+
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+        this.draw()
+
+        const waypoint = waypoints[this.waypointIndex]
+        const yDistance = waypoint.y - this.center.y
+        const xDistance = waypoint.x - this.center.x
+        const angle = Math.atan2(yDistance, xDistance)
+        // console.log(this.position.x)
+
+        // this.position.x += Math.cos(angle)
+        // this.position.y += Math.sin(angle)
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
+
+
+        if (Math.round(this.center.x) === Math.round(waypoint.x) && 
+            Math.round(this.center.y) === Math.round(waypoint.y) && 
+            this.waypointIndex < waypoints.length
+            ){
+            this.waypointIndex++
+        }
+
+
+    }
+}
+
+
+//Building
+
+class Building {
+    constructor({position={x:0, y:0}}){
+        this.position = position
+        this.size = 64 * 2
+    }
+    draw(){
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.size, this.size / 2)
+    }
+}
--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ canvas.height = 768
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+
+
 const image = new Image()
 
 image.onload = () => {
@@ -14,53 +16,35 @@ image.onload = () => {
 }
 image.src = '../img/gameMap.png'
 
-class Enemy {
-    constructor({position, width, height}){
-        this.position = {
-            x: position.x,
-            y: position.y
-        },
-        this.width = width,
-        this.height = height,
-        this.waypointIndex = 0,
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-    }
-
-    draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-
-    update(){
-        this.draw()
-
-        const waypoint = waypoints[this.waypointIndex]
-        const yDistance = waypoint.y - this.center.y
-        const xDistance = waypoint.x - this.center.x
-        const angle = Math.atan2(yDistance, xDistance)
-        // console.log(this.position.x)
-
-        this.position.x += Math.cos(angle)
-        this.position.y += Math.sin(angle)
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
 
 
-        if (Math.round(this.center.x) === Math.round(waypoint.x) && 
-            Math.round(this.center.y) === Math.round(waypoint.y) && 
-            this.waypointIndex < waypoints.length
-            ){
-            this.waypointIndex++
-        }
+const placementTilesData2D = []
 
-
-    }
+for (let i = 0; i < placementTilesData.length; i += 20) {
+  placementTilesData2D.push(placementTilesData.slice(i, i + 20))
 }
+// console.log(placementTilesData2D)
+
+const placementTiles = []
+
+placementTilesData2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 14) {
+      // add building placement tile here
+      placementTiles.push(
+        new PlacementTile({
+          position: {
+            x: x * 64,
+            y: y * 64
+          }
+        })
+      )
+    }
+  })
+})
+
+// console.log(placementTiles)
+
 
 // let x = 200
 
@@ -91,6 +75,9 @@ for ( let i =1; i<10; i++ ){
 }
 
 
+//Select location for buildings and Spawn
+const buildings = []
+let activeTile = undefined
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -98,10 +85,57 @@ function animate(){
 
     enemies.forEach(enemy => enemy.update())
 
+
+    placementTiles.forEach(tile => tile.update(mouse))
+
+    buildings.forEach(building => building.draw())
+
     // c.fillStyle='red'
     // c.fillRect(x, 400, 100, 100)
     // x++
     // console.log('go')
+
+    
 }
 
-animate()
+// animate()
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+
+canvas.addEventListener('click', (event)=>{
+    console.log(buildings)
+
+    if(activeTile && !activeTile.isOccupied){
+        buildings.push(new Building({
+            position: {
+                x: activeTile.position.x,
+                y: activeTile.position.y
+            }
+        }))
+        activeTile.isOccupied = true
+    }
+})
+
+
+window.addEventListener('mousemove', (event)=>{
+    mouse.x = event.clientX,
+    mouse.y = event.clientY
+    // console.log(mouse.x)
+
+    for( let i = 0; i < placementTiles.length; i++ ){
+        const tile = placementTiles[i]
+        if(
+            mouse.x > tile.position.x &&
+            mouse.x < tile.position.x + tile.size &&
+            mouse.y > tile.position.y &&
+            mouse.y < tile.position.y + tile.size
+        ){
+            activeTile = tile
+            break
+        }
+    }
+})
